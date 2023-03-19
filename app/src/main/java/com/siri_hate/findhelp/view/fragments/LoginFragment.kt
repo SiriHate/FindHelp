41d787557f2b56208dba233c0741efbb2c9cea4a
Andrey <70503655+SiriHate@users.noreply.github.com>
2023-03-19 21:20:04 +0300

    v.0.3
    - LoginFragment testing logic added
    - RegisterFragment testing logic added
    - All necessary activities have been added to AndroidManifest

--- app/src/main/java/com/siri_hate/findhelp/view/fragments/LoginFragment.kt
+++ app/src/main/java/com/siri_hate/findhelp/view/fragments/LoginFragment.kt
@@ -1,27 +1,97 @@
 package com.siri_hate.findhelp.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.siri_hate.findhelp.R
+import com.siri_hate.findhelp.view.activities.UserPageActivity
 
 
 class LoginFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    // Переменные для UI
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var loginButton: Button
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        val view = inflater.inflate(R.layout.login_fragment, container, false)
+
+        // Привязка переменных к UI-элементам
+        emailInput = view.findViewById(R.id.Email_input_login)
+        passwordInput = view.findViewById(R.id.Password_input_login)
+        loginButton = view.findViewById(R.id.Login_button)
+
+        // Переменная для Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Слушатель кнопки "Войти" вызывающий функцию авторизации
+        loginButton.setOnClickListener {
+            login()
+        }
+
+        return view
     }
 
+    // Функция авторизации
+    private fun login() {
+        val email = emailInput.text.toString().trim()
+        val password = passwordInput.text.toString().trim()
+
+        var isEmpty = false // Проверка на заполнение поля
+
+        // Проверка на заполнение поля "email"
+        if (email.isEmpty()) {
+            emailInput.error = "Введите email"
+            isEmpty = true
+        }
+
+        // Проверка на заполнение поля "пароль"
+        if (password.isEmpty()) {
+            passwordInput.error = "Введите пароль"
+            isEmpty = true
+        }
+
+        // Если какое-то из полей пустое то выводим ошибку и выходим из функции
+        if (isEmpty) {
+            return
+        }
+
+        // Обработка ошибок авторизации и успешной авторизации
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(requireContext(), UserPageActivity::class.java)
+                    startActivity(intent) // Запуск экрана пользователя
+                    requireActivity().finish()
+                } else {
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(
+                            requireContext(),
+                            "Неверный email или пароль",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "Ошибка авторизации: " + task.exception?.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+    }
 }
--- app/src/main/java/com/siri_hate/findhelp/view/fragments/RegisterFragment.kt
+++ app/src/main/java/com/siri_hate/findhelp/view/fragments/RegisterFragment.kt
@@ -5,22 +5,109 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.siri_hate.findhelp.R
 
 class RegisterFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    // Переменные
+    private lateinit var emailInput: EditText
+    private lateinit var firstPasswordInput: EditText
+    private lateinit var secondPasswordInput: EditText
+    private lateinit var registerButton: Button
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.register_fragment, container, false)
+        val view = inflater.inflate(R.layout.register_fragment, container, false)
+
+        // Переменные
+        emailInput = view.findViewById(R.id.Email_input_register)
+        firstPasswordInput = view.findViewById(R.id.First_password_input_register)
+        secondPasswordInput = view.findViewById(R.id.Second_password_input_register)
+        registerButton = view.findViewById(R.id.Register_button)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
+        // Слушатель кнопки регистрации вызывает функцию регистрации
+        registerButton.setOnClickListener {
+            registration()
+        }
+
+        return view
     }
 
+    // Функция регистрации
+    private fun registration() {
+
+        // Переменные введенных данных
+        val email = emailInput.text.toString().trim()
+        val password = firstPasswordInput.text.toString().trim()
+        val confirmPassword = secondPasswordInput.text.toString().trim()
+
+        var isEmpty = false // Проверка заполнено ли поле
+
+        // Проверка на заполнение поля email
+        if (email.isEmpty()) {
+            emailInput.error = "Введите Email"
+            isEmpty = true
+        }
+
+        // Проверка на заполнение поля пароль
+        if (password.isEmpty()) {
+            firstPasswordInput.error = "Введите пароль"
+            isEmpty = true
+        }
+
+        // Проверка на заполнение поля пароль
+        if (confirmPassword.isEmpty()) {
+            secondPasswordInput.error = "Введите подтверждение пароля"
+            isEmpty = true
+        }
+
+        // Проверка на заполнение подтвержения пароля
+        if (password != confirmPassword) {
+            Toast.makeText(activity, "Пароли не совпадают", Toast.LENGTH_SHORT).show()
+            isEmpty = true
+        }
+
+        // Если какое-то из полей пустое то выводим ошибку и выходим из функции
+        if (isEmpty) {
+            return
+        }
+
+        // Обработка ошибок регистрации и успешная регистрация
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        activity, "Регистрация прошла успешно",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val welcomeFragment = WelcomeFragment()
+                    parentFragmentManager.beginTransaction() // Возврат на приветственный фрагмент
+                        .replace(R.id.AuthorizationFragment, welcomeFragment)
+                        .commit()
+                } else {
+                    if (task.exception is FirebaseAuthUserCollisionException) {
+                        Toast.makeText(
+                            activity,
+                            "Пользователь уже зарегистрирован в системе",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            activity, "Ошибка регистрации: " +
+                                    task.exception?.message, Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+    }
 }
